cec0ed189c261a31aa0eefed730dbf3f185ef6a5
RasuwanK <kalharaweragala@gmail.com>
2024-12-25 02:40:32 +0530

    Basic corner cuts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>E-Commerce store</title>
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+	<main>
+		@yield('main')
+	</main>
+	</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        "./node_modules/flowbite/**/*.js"
+            
     ],
     theme: {
         extend: {
@@ -16,5 +18,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
