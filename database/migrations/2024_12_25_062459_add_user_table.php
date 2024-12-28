2439afd76678bca7dccc5c5db37c13606bffa72d
RasuwanK <kalharaweragala@gmail.com>
2024-12-28 20:56:10 +0530

    New user migrations

--- database/migrations/2024_12_25_062459_add_user_table.php
+++ database/migrations/2024_12_25_062459_add_user_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->string('address');
             $table->string('email')->unique()->nullable();
             $table->string('password'); 
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
