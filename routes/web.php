6af81f0a945b761cb4b3bcc1f512db29385edcf9
RasuwanK <kalharaweragala@gmail.com>
2024-12-29 20:10:33 +0530

    reset

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/signup', function() {
-    return view('signup');
-});
-
-Route::get('/signin', function() {
-    return view('login');
-});
-
-Route::get('/dashboard', function() {
+Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/explore', function() {
-    return view('explore');
-});
-
-Route::get('/products/{id}', function($id) {
-    return view('product', ['id' => $id]);
-});
-
-Route::get('/sellers/{id}', function($id) {
-    return view('seller', ['id' => $id]);
-});
+require __DIR__.'/auth.php';
