1fcc35511edf47e8ab19c4255030633b3e993f99
RasuwanK <kalharaweragala@gmail.com>
2025-01-01 14:00:04 +0530

    New routes for seller and buyer auth

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SellerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::prefix('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'defaultAuth']);    
+
+    Route::get('/seller/signup', [AuthController::class, 'sellerSignUp'])->name('seller.signup');
+
+    Route::post('/seller/signup', [SellerController::class, 'store'])->name('seller.store');
+    
+    Route::get('/buyer/signup', [AuthController::class, 'buyerSignUp'])->name('buyer.signup');
+
+    Route::post('/buyer/signup', [UserController::class, 'store'])->name('buyer.store');
+    
+    Route::get('/signin', [AuthController::class, 'siginIn'])->name('signin');
+});
