6fa35bfcc55d60755737a411983f2a1f928e0953
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-03-07 06:03:06 +0530

    Security project day 2.5

---
+++ app/Models/Discount.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discount extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'id',
+        'code',
+        'discount_percent',
+        'expiration_date',
+    ];
+
+    protected $table = 'discount';
+
+    protected $primaryKey = 'id';
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
